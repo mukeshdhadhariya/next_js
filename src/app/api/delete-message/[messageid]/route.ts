bec8c11ed86199513c53f37b43c2db1c2bfe0adf
mukeshdhadhariya <mukeshdhadhariya1@gmail.com>
2025-08-25 21:41:48 +0530

    error solve to build

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -3,19 +3,24 @@ import { getServerSession } from 'next-auth/next';
 import dbConnect from '@/lib/dbConnect';
 import { User } from 'next-auth';
 import { authOptions } from '../../auth/[...nextauth]/options';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface ExtendedUser extends User {
+  _id: string;
+}
 
 export async function DELETE(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { messageid: string } }
 ) {
-
   const messageId = params.messageid;
   await dbConnect();
+
   const session = await getServerSession(authOptions);
-  const _user: User = session?.user as User;
+  const _user = session?.user as ExtendedUser | undefined;
 
   if (!session || !_user) {
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
@@ -28,19 +33,19 @@ export async function DELETE(
     );
 
     if (updateResult.modifiedCount === 0) {
-      return Response.json(
+      return NextResponse.json(
         { message: 'Message not found or already deleted', success: false },
         { status: 404 }
       );
     }
 
-    return Response.json(
+    return NextResponse.json(
       { message: 'Message deleted', success: true },
       { status: 200 }
     );
   } catch (error) {
     console.error('Error deleting message:', error);
-    return Response.json(
+    return NextResponse.json(
       { message: 'Error deleting message', success: false },
       { status: 500 }
     );
